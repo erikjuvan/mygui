f9a9c045c029fbd4d1a3c4bca0645f9113bd314e
erik.juvan <erik.kiefer.juvan@sensum.eu>
2018-10-15 18:25:26 +0200

    Removed #pragma from cpp file. Corrected Config.hpp to be compilable with gcc on linux.

--- include/mygui/Config.hpp
+++ include/mygui/Config.hpp
@@ -1,7 +1,34 @@
 #pragma once
 
-#if defined(MYGUI_EXPORTS)
-#define MYGUI_API __declspec(dllexport)
+#if defined(_WIN32)
+
+	#define MYGUI_WINDOWS
+
+#elif defined(__unix__)
+
+	#if defined(__linux__)
+		#define MYGUI_LINUX
+	#else
+		#error Only Linux of Unix type OS is supported by mygui library
+	#endif
+
 #else
-#define MYGUI_API __declspec(dllimport)
+
+	#error This operating system is not supported by mygui library
+
+#endif
+
+#if defined(MYGUI_WINDOWS)
+
+	#if defined(MYGUI_EXPORTS)
+		#define MYGUI_API __declspec(dllexport)
+	#else
+		#define MYGUI_API __declspec(dllimport)
+	#endif
+
+#else	// Linux
+
+	#define SFML_API_EXPORT __attribute__((__visibility__("default")))
+	#define SFML_API_IMPORT __attribute__((__visibility__("default")))
+
 #endif
--- src/Button.cpp
+++ src/Button.cpp
@@ -1,5 +1,3 @@
-#pragma once
-
 #include "Button.hpp"
 
 namespace mygui
